d8a478d9dfecd87ffbbdad73833c9247cbb1e411
diwli <1124636246@qq.com>
2016-03-09 15:46:23 +0800

    新增文件

---
+++ testmysql.php
@@ -0,0 +1,8 @@
+<?php 
+print_r(phpinfo());
+$link = mysql_connect('hostname','dbuser','dbpassword'); 
+if (!$link) { 
+	die('Could not connect to MySQL: ' . mysql_error()); 
+} 
+echo 'Connection OK'; mysql_close($link); 
+?> 
